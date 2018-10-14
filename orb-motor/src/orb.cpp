f60918a1ca4e1d4a711d6344e5770d58e2a7df82
Ulysse Vautier <ulysse.vautier@gmail.com>
2018-10-14 19:07:47 +0000

    Added help to orb-upload, testing orb-motor still

--- orb-motor/src/orb.cpp
+++ orb-motor/src/orb.cpp
@@ -1,16 +1,46 @@
 #include <iostream>
 #include <errno.h>
 #include <string>
+#include <getopt.h>
+
 #include <wiringPiI2C.h>
+
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+	int opt;
+	int option_index = 0;
+	
+	int du = 0;
+	int mo = 0;
+	
+	static struct option long_options[] = {
+		{"help",	no_argument, 0,	'h'},
+		{"motor",	required_argument, 0,	'm'},
+		{"duty",	required_argument, 0,	'd'},
+		{0,0,0,0}
+	};
+	while ((opt = getopt_long(argc, argv, "hm:d:",long_options, &option_index)) != -1) {
+		switch (opt) {
+			case 'm':
+				mo = atoi(optarg);
+				break;
+			case 'd':
+				du = atoi(optarg);
+				break;
+			case 'h':
+			default:
+				break;
+		}
+	}
 	int fd, result;
 	fd = wiringPiI2CSetup(0x04);
-	cout << wiringPiI2CRead(fd) << endl;
 
-	//wiringPiI2CWriteReg8(fd, 0x01, 0x00);
+	if(du > 0)
+		cout << wiringPiI2CRead(fd) << endl;
+	if(mo > 0)
+		wiringPiI2CWrite(fd, 0x01);
 	return 0;
 }
--- orb-upload/src/orb.cpp
+++ orb-upload/src/orb.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 #include <errno.h>
 #include <string>
+#include <getopt.h>
 
 using namespace std;
 
+void helper(){
+	cout << "Orb Application Upload :\n
+	Uses Picberry to upload firmwares to the PIC" << endl;
+}
+
 int main(int argc, char *argv[])
 {
+	int opt;
+	int option_index = 0;
+
+	bool h = false;
+
+	static struct option long_option[] = {
+		{"help",	no_argument, 0, 'h'}
+		{0,0,0,0}
+	};
+	
+	while((opt = getopt_long(argc, argv, "h", long_options, &option_index)) != -1) {
+		switch(opt){
+			case 'h':
+			default:
+				h = true;
+				break;
+		}
+	}
+
+	if(h){
+		helper();
+		system(("picberry -h").c_str());
+		return 0;
+	}
+
+
 	string args = "";
 	for(int i = 1; i < argc; ++i)
 		args += string(argv[i]) + " ";
