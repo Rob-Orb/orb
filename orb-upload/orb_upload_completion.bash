c2e119a7510c8ea77f8dfa6fbbeecaa95312d7bb
Ulysse Vautier <ulysse.vautier@gmail.com>
2018-10-28 18:23:34 +0000

    Added bash completion with index online

--- orb-upload/orb_upload_completion.bash
+++ orb-upload/orb_upload_completion.bash
@@ -1,2 +1,22 @@
 #/usr/bin/env bash
-complete -W "motor" upload
+_orb_upload_completion()
+{
+	local cur prev
+
+	#COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+	RESULT="$(wget -qO - https://rob-orb.github.io/firmwares/ls.index | tr '\r\n' ' ')"
+	
+	if [ $COMP_CWORD == 2 ]; then
+		case "$prev" in
+			"upload")
+			COMPREPLY=($(compgen -W "$RESULT" -- $cur));;
+		*)
+		;;
+		esac
+	fi
+	return 0
+}
+#complete -F _orb_upload_completion orb
