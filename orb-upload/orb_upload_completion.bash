8f23d52f7477c4e023d2689c27839f6f71f4deff
Ulysse VAUTIER <ulysse.vautier@gmail.com>
2018-12-11 16:52:43 +0000

    Patched orb-upload

--- orb-upload/orb_upload_completion.bash
+++ orb-upload/orb_upload_completion.bash
@@ -1,22 +1,2 @@
 #/usr/bin/env bash
-_orb_upload_completion()
-{
-	local cur prev
-
-	#COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
-
-	RESULT="$(wget -qO - https://rob-orb.github.io/firmwares/ls.index | tr '\r\n' ' ')"
-	
-	if [ $COMP_CWORD == 3 ]; then
-		case "$prev" in
-			"upload")
-			COMPREPLY=($(compgen -W "$RESULT" -- $cur));;
-		*)
-		;;
-		esac
-	fi
-	return 0
-}
-#complete -F _orb_upload_completion orb
+complete -W "motor" upload
--- orb-upload/src/orb.cpp
+++ orb-upload/src/orb.cpp
@@ -8,17 +8,17 @@ using namespace std;
 
 void helper(){
 	cout << "\nOrb Application Upload Version: " << ORB_VERSION << endl <<
-		"Uses Picberry to upload firmwares to the PIC" << endl << endl <<
-		"Usage: orb upload [firmware] [options]" << endl << endl <<
-		"Options: " << endl <<
-		"-h\t\tprint help" << endl <<
-		"-r [output]\tread chip to file [defaults to ofile.hex] " << endl <<
-		"-w firmware\tbulk erase, write and check firmware" << endl <<
-		"-e\t\terase chip" <<endl <<
-		"-b\t\tblank check of the chip" << endl <<
-		"-d\t\tread configuration registers" << endl <<
-		"--noverify\tskip memory verification after writing" << endl <<
-		"--debug\t\tturn on debug" << endl <<endl;
+	"Uses Picberry to upload firmwares to the PIC" << endl << endl <<
+	"Usage: orb upload [options]" << endl << endl <<
+	"Options: " << endl <<
+	"-h\t\tprint help" << endl <<
+	"-r [output]\tread chip to file [defaults to ofile.hex] " << endl <<
+	"-w firmware\tbulk erase, write and check firmware" << endl <<
+	"-e\t\terase chip" <<endl <<
+	"-b\t\tblank check of the chip" << endl <<
+	"-d\t\tread configuration registers" << endl <<
+	"--noverify\tskip memory verification after writing" << endl <<
+	"--debug\t\tturn on debug" << endl <<endl;
 }
 
 int main(int argc, char *argv[])
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	int option_index = 0;
 
 	bool h = false;
-	bool invalid = true;
+	bool invalid = false;
 
 	static struct option long_options[] = {
 		{"help",	no_argument, 0, 'h'},
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 		{"debug",	no_argument, 0, 1},
 		{0,0,0,0}
 	};
-
+	
 	while((opt = getopt_long(argc, argv, "hw:r:debc", long_options, &option_index)) != -1) {
 		switch(opt){
 			case ':':
@@ -49,7 +49,6 @@ int main(int argc, char *argv[])
 				h = true;
 				break;
 			default:
-				invalid = false;
 				h = false;
 				break;
 		}
@@ -60,21 +59,10 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	if(!invalid){
-		string args = "";
-		for(int i = 1; i < argc; ++i)	
-			args += string(argv[i]) + " ";
-		system(("picberry -f pic16f150x -g 21,20,19 "+args).c_str());
-	}
-	else{
-		string firm = string(argv[optind]);
-		if(firm.back()!='x'){
-			cout << "Gathering firmware " + firm + "..." << endl;
-			system(("wget -q https://rob-orb.github.io/firmwares/"+firm+".hex -O tmp-1234-rob-orb.hex").c_str());
 
-			system("picberry -f pic16f150x -g 21,20,19 -w tmp-1234-rob-orb.hex");
-			system("rm -f tmp-1234-rob-orb.hex");
-		}
-	}
+	string args = "";
+	for(int i = 1; i < argc; ++i)
+		args += string(argv[i]) + " ";
+	system(("picberry -f pic16f150x -g 13,19,26 "+args).c_str());
 	return 0;
 }
